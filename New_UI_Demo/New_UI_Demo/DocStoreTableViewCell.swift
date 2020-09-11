131418847116138ec4b2982ceb2e308c7e7cc7ef
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-09-11 11:18:51 +0530

    Added new UI Project
    - Feature release - NEW UI

---
+++ New_UI_Demo/New_UI_Demo/DocStoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DocStoreTableViewCell.swift
+//  New_UI_Demo
+//
+//  Created by Mallikarjun on 11/09/20.
+//  Copyright © 2020 Mallikarjun. All rights reserved.
+//
+
+import UIKit
+
+class DocStoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ New_UI_Demo/New_UI_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New_UI_Demo
+//
+//  Created by Mallikarjun on 11/09/20.
+//  Copyright © 2020 Mallikarjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
