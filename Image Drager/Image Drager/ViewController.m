10233fbcbeefce787eb25940a8ae142b744fce91
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-09-30 11:45:33 +0530

    Draging code added
    User can able to move/drag the imageview to any spefific location into the screen

--- Image Drager/Image Drager/ViewController.m
+++ Image Drager/Image Drager/ViewController.m
@@ -9,15 +9,57 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIImageView * imageview;
+    CGRect frame;
+}
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    CGRect myImageRect = CGRectMake(100, 100, 200, 35);
+    self->imageview = [[UIImageView alloc] initWithFrame:myImageRect];
+    [self->imageview setImage:[UIImage imageNamed:@"signer"]];
+    [self.view addSubview:self->imageview];
+
+}
+ 
+//This method is use - If u select/touch anywhere in the screen that image will move
+
+/*- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchLocation = [touch locationInView:touch.view];
+    self->imageview.center = touchLocation;
+
+    frame = [self.view convertRect:self->imageview.frame fromView:self.view];
+    NSLog(@"touchesBegan %@", CGRectCreateDictionaryRepresentation(frame));
+    
+    if ([touch.view isEqual: self.view] || touch.view == nil) {
+        return;
+    }
+
+}
+*/
+
+//This method use - only if, if u want to drag the view only when u move that imageview and then it will change its co-ordinated
+
+- (void)touchesMoved:(NSSet*)touches withEvent:(UIEvent*)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchLocation = [touch locationInView:touch.view];
+
+    frame = [self.view convertRect:self->imageview.frame fromView:self.view];
+    NSLog(@"touchesMoved %@", CGRectCreateDictionaryRepresentation(frame));
+    if ([touch.view isEqual: self.view]) {
+
+        self->imageview.center = touchLocation;
+
+        return;
+    }
 }
 
-
 @end
+
