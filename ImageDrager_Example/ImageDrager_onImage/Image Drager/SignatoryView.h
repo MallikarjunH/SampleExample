91a0227060afbf075895dbe56e7c02670e645202
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-10-01 13:02:45 +0530

    Drag Image in ImageView
    Added example of dragging/moving image from ImageView

---
+++ ImageDrager_Example/ImageDrager_onImage/Image Drager/SignatoryView.h
@@ -0,0 +1,17 @@
+//
+//  SignatoryView.h
+//  Image Drager
+//
+//  Created by Mallikarjun on 01/10/20.
+//  Copyright © 2020 Nikita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignatoryView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
