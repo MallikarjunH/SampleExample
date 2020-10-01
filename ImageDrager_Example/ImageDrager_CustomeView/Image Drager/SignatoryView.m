77d4885079079fcd40f864c8365312939749fb6b
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-10-01 13:06:00 +0530

    Drag CustomeView on ImageView

---
+++ ImageDrager_Example/ImageDrager_CustomeView/Image Drager/SignatoryView.m
@@ -0,0 +1,21 @@
+//
+//  SignatoryView.m
+//  Image Drager
+//
+//  Created by Mallikarjun on 01/10/20.
+//  Copyright © 2020 Nikita. All rights reserved.
+//
+
+#import "SignatoryView.h"
+
+@implementation SignatoryView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
